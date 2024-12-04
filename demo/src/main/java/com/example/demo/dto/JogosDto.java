718a3a464d79137f6e04e0f9d6044217d74a8ff0
GustavoGomes-dev <gustavo10.ggj@gmail.com>
2024-12-04 11:11:11 -0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/dto/JogosDto.java
@@ -0,0 +1,5 @@
+package com.example.demo.dto;
+
+public record JogosDto(String nome,String categoria, String classificacao,String avaliacao, String lancamento, String descricao) {
+
+}
---
+++ demo/src/main/java/com/example/demo/dto/UpdateJogoDto.java
@@ -0,0 +1,4 @@
+package com.example.demo.dto;
+
+public record UpdateJogoDto(String nome,String categoria, String classificacao,String avaliacao, String lancamento, String descricao) {
+	}
